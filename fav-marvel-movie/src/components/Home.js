043e13f7dddaa6785b82bbf285e5dfc0d1b2e2a0
Margie Ball <margie_ball@glic.com>
2021-06-19 22:57:22 -0400

    added homepage styling

--- fav-marvel-movie/src/components/Home.js
+++ fav-marvel-movie/src/components/Home.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
+import marvelgif from '../marvelgif.gif';
 
 
 const Home = () => {
     return (
-        <div>
+        <div className="homepage">
             <div className="navigation" id="home">
-                <h1 className="home-header">Welcome to MARVEL Movie Mania!</h1>
-
-                <ul>
-                    <Link to="/login"><li>Login</li></Link>
-                    <Link to="/signup"><li>Sign-Up</li></Link>
-                </ul>
+                <Link to="/login"><h3>Login</h3></Link>
+                <Link to="/signup"><h3>Sign-Up</h3></Link>
 
             </div>
 
-            <div className="body">
+            <div className="home-body">
+                <h1 className="home-header">Welcome to MARVEL Movie Mania!</h1>
+                <img className="marvel-img" src={marvelgif} alt="marvellogo" />
 
 
             </div>
